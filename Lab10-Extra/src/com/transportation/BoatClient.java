646e2bf0099f7b33a9f1bcbd1d1c2b9e653e1ade
Gabriel Viox <GabrielV@tlgcohort.com>
2024-05-31 13:57:54 -0500

    10.1 extra try-catch-finally-throws practice and review. Examined options 1-4

--- Lab10-Extra/src/com/transportation/BoatClient.java
+++ Lab10-Extra/src/com/transportation/BoatClient.java
@@ -1,7 +1,7 @@
 package com.transportation;
 
 public class BoatClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DestinationUnreachableException {
         Boat boat = new Boat("Ella Belle");
         boat.moveTo("North Austin");
     }
